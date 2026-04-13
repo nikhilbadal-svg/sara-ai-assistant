e85e61dc63d708e68526c0839f73d1f0a9831f0d
nikhilbadal-svg <nikhilgangster6696@gmail.com>
2026-04-13 21:23:59 +0530

    Delete sara-ai-assistant directory

--- sara-ai-assistant/sara/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SARA (Smart Autonomous Responsive Assistant)
-Created by Nikhil Badal
-"""
-
-__version__ = "1.0.0"
-__author__ = "Nikhil Badal"
-__description__ = "Smart Autonomous Responsive Assistant"
